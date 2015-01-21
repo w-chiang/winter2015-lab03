dca523b4377bfa4cbadb9b69ed863eac7a4c188c
w-chiang <williampwc@gmail.com>
2015-01-20 16:49:58 -0800

    added routes to serve wise/bingo and comp4711/wisdom added bingo controller and wisdom method
    Signed-off-by:w-chiang <williampwc@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -44,6 +44,8 @@ $route['sleep'] = "first/zzz";
 $route['lock/(:any)/(:any)'] = "welcome/shucks";
 $route['show/(:num)'] = "first/gimme/$1";
 $route['dunno'] = "guess";
+$route['([a-zA-Z]{4})/bingo'] = "bingo";
+$route['comp(\d+)/(.*)'] = "bingo/wisdom";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
---
+++ application/controllers/Bingo.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/First.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Bingo extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $source = $this->quotes->get('5');
+        
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        $this->data['mug'] = $source['mug'];
+
+        $this->render();
+    }
+    
+    function wisdom() {
+        $this->data['pagebody'] = 'justone';
+        $source = $this->quotes->get('6');
+        
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        $this->data['mug'] = $source['mug'];
+
+        $this->render();
+    }
+
+}
+
+/* End of file First.php */
+/* Location: application/controllers/First.php */
